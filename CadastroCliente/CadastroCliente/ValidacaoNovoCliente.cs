bba5ca443646935271daf6fda15a7c8c74b4e739
Luiz Henrique C. Correa <luizhcorrea@gmail.com>
2016-01-19 17:24:23 -0200

    Estruturação e entendimento do modelo

---
+++ CadastroCliente/CadastroCliente/ValidacaoNovoCliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CadastroCliente
+{
+    [TestClass]
+    public class ValidacaoNovoCliente
+    {
+        
+        [TestMethod]
+        public void DeveValidarCNPJNovoCliente()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeveValidarCPFSocios()
+        {
+            
+        }
+    }
+}
